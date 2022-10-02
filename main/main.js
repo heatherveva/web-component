af097f8719284f91d622c0a970b353c9afebc871
Heather Stevens <heatherveva@gmail.com>
2022-10-02 18:42:05 -0500

    button styling

--- main/main.js
+++ main/main.js
@@ -2,15 +2,19 @@ const template = document.createElement("template");
 template.innerHTML = `
   <style>
     button {
+      border-style: none;
       margin-top: 20px;
       color: white;
       font-weight: bold;
-      background-color: black;
+      background-color: #CB3036;
+      font-style: bold;
+      font-family: Arial, Helvetica, sans-serif;
+      padding: 10px 25px 10px 25px;
     }
   </style>
   <div>
-  <a href="https://www.google.com" target="_blank">
- <button>SEARCH</button>
+  <a href="https://www.bitovi.com/" target="_blank">
+ <button>Visit Bitovi</button>
  </a>
   </div>
 `;
